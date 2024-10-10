d3a84716cd26091b9a29dcceeee333f8c79b81cc
FVancevic <fvancevic@frba.utn.edu.ar>
2024-10-10 12:26:17 -0300

    TransactionService - Metodo getTotalAmount

--- src/main/java/com/example/TransactionService.java
+++ src/main/java/com/example/TransactionService.java
@@ -1,8 +1,10 @@
 package com.example;
 
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
@@ -22,5 +24,23 @@ public class TransactionService {
                 .filter(t -> t.getType().equals(type))
                 .map(Transaction::getId)
                 .collect(Collectors.toList());
+    }
+
+    public double getTotalAmount(long transactionId){
+        double sum = 0;
+
+        Queue<Long> queue = new LinkedList<>();
+        queue.add(transactionId);
+
+        while(!queue.isEmpty()){
+            long currentId = queue.poll();
+            Transaction currentTransaction = transactionsMap.get(currentId);
+            sum += currentTransaction.getAmount();
+
+            transactionsMap.values().stream()
+                    .filter(t -> t.getParentId() == currentTransaction.getId())
+                    .forEach(t -> queue.add(t.getId()));
+        }
+        return sum;
     }
 }
